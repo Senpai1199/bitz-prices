e651e73abab5a8087ab7da6bed554ee508fda600
Divyansh Agarwal <divyansh1199@gmail.com>
2020-09-26 00:03:01 +0530

    Finish application

---
+++ components/Prices.js
@@ -0,0 +1,66 @@
+class Prices extends React.Component {
+  state = {
+    currency: "EUR",
+  };
+
+  render() {
+    let list = "";
+
+    switch (this.state.currency) {
+      case "USD":
+        list = (
+          <li className="list-group-item">
+            Bitcoin rate for {this.props.bpi.USD.description}:{" "}
+            <span className="badge badge-primary">
+              {this.props.bpi.USD.code}
+            </span>{" "}
+            {"  "}
+            <strong>{this.props.bpi.USD.rate}</strong>
+          </li>
+        );
+        break;
+      case "GBP":
+        list = (
+          <li className="list-group-item">
+            Bitcoin rate for {this.props.bpi.GBP.description}:{" "}
+            <span className="badge badge-primary">
+              {this.props.bpi.GBP.code}
+            </span>{" "}
+            {"  "}
+            <strong>{this.props.bpi.GBP.rate}</strong>
+          </li>
+        );
+        break;
+      case "EUR":
+        list = (
+          <li className="list-group-item">
+            Bitcoin rate for {this.props.bpi.EUR.description}:{" "}
+            <span className="badge badge-primary">
+              {this.props.bpi.EUR.code}
+            </span>{" "}
+            {"  "}
+            <strong>{this.props.bpi.EUR.rate}</strong>
+          </li>
+        );
+        break;
+    }
+
+    return (
+      <div>
+        <ul className="list-group">{list}</ul>
+        <br />
+        <p>Choose currency: </p>
+        <select
+          onChange={(e) => this.setState({ currency: e.target.value })}
+          className="form-control"
+        >
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+          <option value="EUR">EUR</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default Prices;
---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>
+        This app fetches the current Bitcoin rates in USD, EUR and GBP
+        currencies.
+      </h1>
+    </div>
+  </Layout>
+);
+
+export default About;
